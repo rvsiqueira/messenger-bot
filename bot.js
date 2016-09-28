a25080497be9ed972ffef3dc6556ed7f28c840d5
Rafael Siqueira <rafaelsiqueira@Rafaels-MacBook-Pro-2.local>
2016-09-28 13:45:16 +0530

    adding some actions to bot

--- bot.js
+++ bot.js
@@ -56,11 +56,18 @@ const actions = {
   },
   merge(sessionId, context, entities, message, cb) {
     // Retrieve the location entity and store it into a context field
-    const loc = firstEntityValue(entities, 'location');
-    if (loc) {
-      context.loc = loc; // store it in context
+    if (firstEntityValue(entities, 'insurance')) {
+      context.insurance = firstEntityValue(entities, 'insurance'); // store it in context
     }
-
+    if (firstEntityValue(entities, 'name')) {
+      context.name = firstEntityValue(entities, 'name');; // store it in context
+    }
+    if (firstEntityValue(entities, 'age')) {
+      context.age = firstEntityValue(entities, 'age'); // store it in context
+    }
+    if (firstEntityValue(entities, 'carModel')) {
+      context.carModel = firstEntityValue(entities, 'carModel'); // store it in context
+    }
     cb(context);
   },
 
@@ -69,34 +76,10 @@ const actions = {
   },
 
   // fetch-weather bot executes
-  ['fetch-weather'](sessionId, context, cb) {
+  ['fetch-insurance'](sessionId, context, cb) {
     // Here should go the api call, e.g.:
     // context.forecast = apiCall(context.loc)
-    context.forecast = 'sunny';
-    cb(context);
-  },
-  // fetch-weather bot executes
-  ['getInsurance'](sessionId, context, cb) {
-    // Here should go the api call, e.g.:
-    // context.forecast = apiCall(context.loc)
-    cb(context);
-  },
-  // fetch-weather bot executes
-  ['getName'](sessionId, context, cb) {
-    // Here should go the api call, e.g.:
-    // context.forecast = apiCall(context.loc)
-    cb(context);
-  },
-  // fetch-weather bot executes
-  ['getEmail'](sessionId, context, cb) {
-    // Here should go the api call, e.g.:
-    // context.forecast = apiCall(context.loc)
-    cb(context);
-  },
-  // fetch-weather bot executes
-  ['getAge'](sessionId, context, cb) {
-    // Here should go the api call, e.g.:
-    // context.forecast = apiCall(context.loc)
+    context.insuranceValue = 'R$100';
     cb(context);
   },
 };
